c064fe05d1903e2e3b8f692824fd5e826a665ae5
Pham Tuan <jvgiveup@gmail.com>
2020-06-06 10:46:42 +0700

    update

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -25,36 +25,43 @@ let getFacebookUsername = (sender_psid) => {
 
 let sendResponseWelcomeNewCustomer = (username, sender_psid) => {
     return new Promise(async (resolve, reject) => {
-        let response_first = {"text": `Welcome ${username} to HaryPhamDev's Restaurant`};
-        let response_second = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [ {
-                        "title": "HaryPhamDev 's restaurant",
-                        "image_url": "https://bit.ly/imageToSend",
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Main menu",
-                                "payload": "MENU",
-                            }
-                        ],
-                    } ]
+        try{
+            let response_first = {"text": `Welcome ${username} to HaryPhamDev's Restaurant`};
+            let response_second = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [ {
+                            "title": "HaryPhamDev 's restaurant",
+                            "image_url": "https://bit.ly/imageToSend",
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "Main menu",
+                                    "payload": "MENU",
+                                }
+                            ],
+                        } ]
+                    }
                 }
-            }
-        };
+            };
 
-        //send a welcome message
-        await sendMessage(sender_psid, response_first);
+            //send a welcome message
+            await sendMessage(sender_psid, response_first);
 
-        //send a image with button view main menu
-        await sendMessage(sender_psid, response_second);
+            //send a image with button view main menu
+            await sendMessage(sender_psid, response_second);
+
+            resolve("done!")
+        }catch (e) {
+            reject(e);
+        }
+
     });
 };
 
-let sendMessage = (sender_id, response) => {
+let sendMessage = (sender_psid, response) => {
     let request_body = {
         "recipient": {
             "id": sender_psid
