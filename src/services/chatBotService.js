03f4e7b916b155035a36233499149d891d1ec4d1
Pham Tuan <jvgiveup@gmail.com>
2020-06-06 12:05:37 +0700

    update

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -28,21 +28,84 @@ let sendResponseWelcomeNewCustomer = (username, sender_psid) => {
         try{
             let response_first = {"text": `Welcome ${username} to HaryPhamDev's Restaurant`};
             let response_second = {
+                // "attachment": {
+                //     "type": "template",
+                //     "payload": {
+                //         "template_type": "generic",
+                //         "elements": [ {
+                //             "title": "HaryPhamDev 's restaurant",
+                //             "image_url": "https://bit.ly/imageToSend",
+                //             "buttons": [
+                //                 {
+                //                     "type": "postback",
+                //                     "title": "Main menu",
+                //                     "payload": "MENU",
+                //                 }
+                //             ],
+                //         } ]
+                //     }
+                // }
                 "attachment": {
                     "type": "template",
                     "payload": {
-                        "template_type": "generic",
-                        "elements": [ {
-                            "title": "HaryPhamDev 's restaurant",
-                            "image_url": "https://bit.ly/imageToSend",
-                            "buttons": [
-                                {
-                                    "type": "postback",
-                                    "title": "Main menu",
-                                    "payload": "MENU",
+                        "template_type": "list",
+                        "top_element_style": "compact",
+                        "elements": [
+                            {
+                                "title": "Classic T-Shirt Collection",
+                                "subtitle": "See all our colors",
+                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                                "buttons": [
+                                    {
+                                        "title": "View",
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "Classic White T-Shirt",
+                                "subtitle": "See all our colors",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                                    "messenger_extensions": false,
+                                    "webview_height_ratio": "tall"
                                 }
-                            ],
-                        } ]
+                            },
+                            {
+                                "title": "Classic Blue T-Shirt",
+                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                                "subtitle": "100% Cotton, 200% Comfortable",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                                    "messenger_extensions": true,
+                                    "webview_height_ratio": "tall",
+                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                },
+                                "buttons": [
+                                    {
+                                        "title": "Shop Now",
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                    }
+                                ]
+                            }
+                        ],
+                        "buttons": [
+                            {
+                                "title": "View More",
+                                "type": "postback",
+                                "payload": "payload"
+                            }
+                        ]
                     }
                 }
             };
