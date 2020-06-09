c176a054bf18c1920098d29bf8a507b99a66ea27
Pham Tuan <jvgiveup@gmail.com>
2020-06-09 16:34:25 +0700

    update

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -422,28 +422,33 @@ let handleReserveTable = (sender_psid) => {
 };
 
 let sendMessage = (sender_psid, response) => {
-    let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": response
-    };
+    return new Promise((resolve, reject) => {
+        try{
+            let request_body = {
+                "recipient": {
+                    "id": sender_psid
+                },
+                "message": response
+            };
 
-    // Send the HTTP request to the Messenger Platform
-    request({
-        "uri": "https://graph.facebook.com/v6.0/me/messages",
-        "qs": { "access_token": PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        console.log(res);
-        console.log(body)
-        if (!err) {
-            console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
+            // Send the HTTP request to the Messenger Platform
+            request({
+                "uri": "https://graph.facebook.com/v6.0/me/messages",
+                "qs": { "access_token": PAGE_ACCESS_TOKEN },
+                "method": "POST",
+                "json": request_body
+            }, (err, res, body) => {
+                if (!err) {
+                    resolve("done")
+                } else {
+                    reject("Unable to send message:" + err);
+                }
+            });
+        }catch (e) {
+            reject(e);
         }
-    });
+    })
+
 };
 
 let sendMessageAskingQuality = (sender_id) => {
